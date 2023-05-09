eb409b2178fcff4a26907da07232c5cc568ed17e
ellesonra <agabales@77global.biz>
2023-05-09 16:58:21 +0800

    added_student_edit_profile

--- apps/sitlms_app/urls.py
+++ apps/sitlms_app/urls.py
@@ -35,6 +35,7 @@ urlpatterns = [
     path('course/delete_course/<int:id>', course.delete_course, name='delete_course'),
     path('student_profile', views.student_profile,name="student_profile"),
     path('student_profile/course_details/', views.student_course_details,name="course_details"),
+     path('student_profile/edit_profilw/', views.student_edit_profile,name="edit_profile"),
     
 ]
 
--- apps/sitlms_app/views.py
+++ apps/sitlms_app/views.py
@@ -132,3 +132,10 @@ def student_course_details(request):
     """ This function renders the student page course details """
     
     return render(request, 'student_module/courses_details.html')
+
+
+def student_edit_profile(request):
+    
+    """ This function renders the student edit profile"""
+    
+    return render(request, 'student_module/edit_profile.html')
