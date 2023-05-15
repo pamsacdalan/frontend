44d90add80e87699148ad7651aeaeb0181524022
jroberto <jroberto@UBVPNN2815.core.unionbank.com>
2023-05-15 14:16:04 +0800

    added view courses of certain instructor

--- apps/sitlms_instructor/urls.py
+++ apps/sitlms_instructor/urls.py
@@ -7,5 +7,6 @@ from apps.sitlms_instructor import views
 
 urlpatterns = [
     path('instructor', views.instructor,name="instructor"),
+    path('instructor/view_courses', views.instructor_view_enrolled_course, name='view_courses'),
    
 ]
