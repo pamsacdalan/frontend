7b129fdc4e43f5eadc16e4a57b4c1560705d2c0a
jqbmaranan <james.maranan.512@gmail.com>
2023-05-23 14:01:38 +0800

    Init student module access tests

--- apps/sitlms_student/urls.py
+++ apps/sitlms_student/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('student_profile/course_details/', views.student_course_details,name="course_details"),
     path('student_profile/edit_profile/', views.student_edit_profile,name="edit_profile"),
     path('student_profile/view_courses/<str:id>', views.student_view_course, name='view_courses'),
+    path('student-no-access',views.custom_403_2,name="student-no-access"),
 ]
--- apps/sitlms_student/views.py
+++ apps/sitlms_student/views.py
@@ -1,11 +1,39 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import get_user_model
 from apps.sitlms_app.models import Course_Catalog, Course_Enrollment, Student_Enrollment, Students_Auth
 from django.template import loader
+from django.core.exceptions import PermissionDenied
+from django.contrib.auth.decorators import user_passes_test
 # Create your views here.
 
+def is_student(user):
+    try:
+        if hasattr(user,'students_auth'):
+            return True
+        raise PermissionDenied
+    except Exception as e:
+        raise PermissionDenied
+    
+def is_correct_student_cbatch_id(student, cbatch_id):
+    try:
+        # print(student)
+        # print(Student_Enrollment.objects.filter(course_batch=cbatch_id).values('student_id'))
+        # print('-')
+        if Student_Enrollment.objects.filter(course_batch=cbatch_id, student_id=student).exists():
+            pass
+        else:
+            return True
+    except Exception as e:
+        return True
 
+@user_passes_test(is_student)   
+def custom_403_2(request):
+    # Pag pinasa ko with status=403, ayaw magload ng page :(
+    # Dapat ganito: render(request, 'custom_403_1.html', status=403)
+    return render(request, 'custom_403_2.html')
+
+@user_passes_test(is_student)   
 def student_profile(request):
     
     user = request.user
@@ -23,7 +51,10 @@ def student_profile(request):
                 }
     return render(request,'student_module/student.html',context)
 
+@user_passes_test(is_student)   
 def student_view_course(request,id):
+    if is_correct_student_cbatch_id(request.user.students_auth, id):
+        return redirect("student-no-access")
     enrolled_class = Student_Enrollment.objects.filter(course_batch=id)
     # print(enrolled_class)
     context={
@@ -31,14 +62,14 @@ def student_view_course(request,id):
     }
     return render(request,'student_module/view_courses.html',context)
 
-
+@user_passes_test(is_student)   
 def student_course_details(request):
     
     """ This function renders the student page course details """
     
     return render(request, 'student_module/courses_details.html')
 
-
+@user_passes_test(is_student)   
 def student_edit_profile(request):
     
     """ This function renders the student edit profile"""
